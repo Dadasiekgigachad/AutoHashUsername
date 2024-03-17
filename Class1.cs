ccf701d00673178af18464ce266de2c2ba404683
Dadasiekgigachad <155850628+Dadasiekgigachad@users.noreply.github.com>
2024-03-17 21:59:54 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Witaju.Serwis;
+
+namespace Witaju
+{
+    public class Class1 : RocketPlugin<Config>
+    {
+        public static Class1 Instance {  get; set; }
+        protected override void Load()
+        {
+            Instance = this;
+            UnturnedPermissions.OnJoinRequested += Connecting.OnConnecting;
+            U.Events.OnPlayerConnected += Join.Joining;
+        }
+        protected override void Unload()
+        {
+            UnturnedPermissions.OnJoinRequested -= Connecting.OnConnecting;
+            U.Events.OnPlayerConnected -= Join.Joining;
+        }
+        
+    }
+}
---
+++ Serwis/Connecting.cs
@@ -0,0 +1,30 @@
+﻿using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Witaju
+{
+    internal class Connecting
+    {
+        internal static void OnConnecting(CSteamID player, ref ESteamRejection? rejectionReason)
+        {
+            SteamPending steam = Provider.pending.FirstOrDefault((SteamPending p) => p.playerID.steamID.m_SteamID == player.m_SteamID);
+
+            int Name = Provider.clients.Count+1;
+            foreach (SteamPlayer argplayer in Provider.clients)
+            {
+                if(argplayer.playerID.characterName == Name.ToString())
+                {
+                    Name++;
+                    break;
+                }
+            }
+            steam.playerID.characterName = "#Name" + Name.ToString();
+            steam.playerID.nickName = "#Name" + Name.ToString();
+        }
+    }
+}
